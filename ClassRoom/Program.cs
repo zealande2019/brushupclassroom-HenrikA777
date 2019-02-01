e2b3a6f44e9eb70d78679fc267424830a5f401fa
Henrik Andersen <henr563a@edu.easj.dk>
2019-02-01 14:02:14 +0100

    Done Done

--- ClassRoom/Program.cs
+++ ClassRoom/Program.cs
@@ -10,12 +10,14 @@ namespace ClassRoom
     {
         static void Main(string[] args)
         {
-            KlasseRum klasse3r = new KlasseRum("3R", new List<Studerende>() {new Studerende("Henrik", 4, 5), new Studerende("Mads", 11, 26) }, new DateTime(2018, 9, 4));
+            KlasseRum klasse3r = new KlasseRum("3R", new List<Studerende>() {new Studerende("Henrik", 4, 5), new Studerende("Mads", 11, 26), new Studerende("Simon", 12, 23), new Studerende("Marcel", 6, 12) }, new DateTime(2018, 9, 4));
             Console.WriteLine(klasse3r.ToString());
             foreach (var studerende in klasse3r.StuderendeListe)
             {
                 Console.WriteLine(studerende.Navn + "   ||  " + studerende.Årstid);
             }
+
+            Console.WriteLine();
             klasse3r.PrintÅrstider();
 
             Console.ReadLine();
